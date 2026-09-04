c05237a8ff997dd1a8e472a06db4aa6d3c71c56e
aryanrajssm660 <aryanrajssm660@gmail.com>
2026-09-04 08:27:54 +0530

    Time: 11 ms (81.15%) | Memory: 56.9 MB (90.34%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,31 +1,48 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+
+        return dummy.next;
+    }
+
     ListNode* sortList(ListNode* head) {
-        vector<int> arr;
-        ListNode* curr = head;
-        while (curr != NULL) {
-            arr.push_back(curr->val);
-            curr = curr->next;
-        }
-        sort(arr.begin(), arr.end());
+        if (!head || !head->next)
+            return head;
 
-        curr = head;
-        int i = 0;
-        while (curr) {
+        // Find the middle
+        ListNode* slow = head;
+        ListNode* fast = head->next;
 
-            curr->val = arr[i++];
-            curr = curr->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return head;
+
+        // Split into two halves
+        ListNode* right = slow->next;
+        slow->next = nullptr;
+
+        // Sort both halves
+        ListNode* left = sortList(head);
+        right = sortList(right);
+
+        // Merge sorted halves
+        return merge(left, right);
     }
 };
