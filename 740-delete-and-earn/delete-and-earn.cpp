fbd87fe2cc2c554060d56620c6f6443cc6e16436
aryanrajssm660 <aryanrajssm660@gmail.com>
2026-09-05 01:03:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (66.46%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        map<int, int> mpp;
-
-        for (auto it : nums) {
-            mpp[it]++;
+        int mx=*max_element(nums.begin(),nums.end());
+        vector<int>arr(mx+1,0);
+        for(auto it:nums){
+            arr[it]+=it;
         }
-
-        int size = mpp.size();
-        vector<int> dp(size);
-
-        int i = 0;
-        int prev = -1;
-
-        for (auto it : mpp) {
-            int curr = it.first;
-            int value = curr * it.second;
-
-            if (i == 0) {
-                dp[i] = value;
-            }
-            else if (curr - prev > 1) {
-                dp[i] = dp[i - 1] + value;
-            }
-            else {
-                dp[i] = value;
-
-                if (i >= 2) {
-                    dp[i] = max(dp[i], dp[i - 2] + value);
-                }
-
-                dp[i] = max(dp[i], dp[i - 1]);
-            }
-
-            prev = curr;
-            i++;
+        vector<int>dp(mx+1,0);
+        dp[1]=arr[1];
+        dp[0]=arr[0];
+        for(int i=2;i<=mx;i++){
+            dp[i]=max(dp[i-1],arr[i]+dp[i-2]);
         }
-
-        return dp[size - 1];
+        return dp[mx];
     }
 };
