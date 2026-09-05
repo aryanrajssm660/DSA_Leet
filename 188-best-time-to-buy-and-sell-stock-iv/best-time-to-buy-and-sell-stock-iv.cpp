9076aebb5e4c99c33195a72a8f14aa69d4e6a37b
aryanrajssm660 <aryanrajssm660@gmail.com>
2026-09-06 03:52:40 +0530

    Time: 26 ms (11.51%) | Memory: 19.4 MB (12.82%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int find(int index, int CanSell, int k, vector<int>& prices,
-    vector<vector<vector<int>>> &dp) {
+             vector<vector<vector<int>>>& dp) {
         if (index == prices.size() || k == 0) {
             return 0;
         }
-        if(dp[index][CanSell][k]!=-1){
+        if (dp[index][CanSell][k] != -1) {
             return dp[index][CanSell][k];
         }
+        int buy = 0, sell = 0, hold;
         if (CanSell) {
-            int Sell = prices[index] + find(index + 1, 0, k - 1, prices,dp);
-            int hold = find(index + 1, 1, k, prices,dp);
-            return dp[index][CanSell][k]=max(Sell, hold);
+             sell = prices[index] + find(index + 1, 0, k - 1, prices, dp);
+
         } else {
-            int buy = -prices[index] + find(index + 1, 1, k, prices,dp);
-            int hold = find(index + 1, 0, k, prices,dp);
-            return dp[index][CanSell][k]=max(buy, hold);
+            buy = -prices[index] + find(index + 1, 1, k, prices, dp);
         }
-        return 0;
+        hold = find(index + 1, CanSell, k, prices, dp);
+        return dp[index][CanSell][k] = max({buy, sell, hold});
     }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n,
-                                       vector<vector<int>>(2, vector<int>(k + 1, -1)));
-        return find(0, 0, k, prices,dp);
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+        return find(0, 0, k, prices, dp);
     }
 };
