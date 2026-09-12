76cbf4d6a6dbbdb97d090dcec6c6ed35a672b1e6
aryanrajssm660 <aryanrajssm660@gmail.com>
2026-09-12 18:16:58 +0530

    Time: 408 ms (39.34%) | Memory: 211.6 MB (39.71%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int solve(int index, int canSell, int cap,
-              vector<int>& prices,
+    int solve(int index, int canSell, int cap, vector<int>& prices,
               vector<vector<vector<int>>>& dp) {
 
         if (index == prices.size() || cap == 0) {
@@ -16,16 +15,13 @@ public:
 
         if (canSell) {
             // Sell the stock
-            take = prices[index] +
-                   solve(index + 1, 0, cap - 1, prices, dp);
+            take = prices[index] + solve(index + 1, 0, cap - 1, prices, dp);
 
             // Hold the stock
             skip = solve(index + 1, 1, cap, prices, dp);
-        }
-        else {
+        } else {
             // Buy the stock
-            take = -prices[index] +
-                   solve(index + 1, 1, cap, prices, dp);
+            take = -prices[index] + solve(index + 1, 1, cap, prices, dp);
 
             // Do not buy
             skip = solve(index + 1, 0, cap, prices, dp);
@@ -38,9 +34,36 @@ public:
         int n = prices.size();
 
         vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(2, vector<int>(3, -1))
-        );
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        // for(int i=0;i<2;i++){
+        //     for(int j=0;j<3;j++){
+        //         dp[n][i][j]=0;
+        //     }
+        // }
+        for (int index = n - 1; index >= 0; index--) {
+            for (int j = 0; j < 2; j++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    int take, skip;
+                    if (j) {
+                        // Sell the stock
+                        take = prices[index] +
+                               dp[index + 1][0][cap - 1];
 
-        return solve(0, 0, 2, prices, dp);
+                        // Hold the stock
+                        skip = dp[index + 1][1][cap];
+                    } else {
+                        // Buy the stock
+                        take = -prices[index] + dp[index + 1][1][cap];
+
+                        // Do not buy
+                        skip = dp[index + 1][0][cap];
+                    }
+
+                    dp[index][j][cap] = max(take, skip);
+                }
+            }
+        }
+
+        return dp[0][0][2];
     }
 };
