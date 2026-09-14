b05056e6ac6eb2d3136d8f8b3e7da13743dd5b56
aryanrajssm660 <aryanrajssm660@gmail.com>
2026-09-14 10:33:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (52.02%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int find(int index, int canbuy, vector<int>& prices,
-             vector<vector<int>>& dp) {
-        if (index >= prices.size()) {
-            return 0;
-        }
-        if(dp[index][canbuy]!=-1){
-            return dp[index][canbuy];
-        }
-        int take, skip;
-        if (canbuy == 0) {
-            // zero indicates buy
-            take = -prices[index] + find(index + 1, 1, prices,dp);
-            skip = find(index + 1, 0, prices,dp);
-        } else {
-            // 1 indicate sell
-            take = prices[index] + find(index + 2, 0, prices,dp);
-            skip = find(index + 1, 1, prices,dp);
-        }
-        return dp[index][canbuy] = max(take, skip);
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
-        return find(0, 0, prices, dp);
+
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int canbuy = 0; canbuy < 2; canbuy++) {
+
+                int take, skip;
+
+                if (canbuy == 0) {
+                    // 0 = buy
+                    take = -prices[index] + dp[index + 1][1];
+                    skip = dp[index + 1][0];
+                } else {
+                    // 1 = sell
+                    take = prices[index] + dp[index + 2][0];
+                    skip = dp[index + 1][1];
+                }
+
+                dp[index][canbuy] = max(take, skip);
+            }
+        }
+
+        return dp[0][0];
     }
 };
